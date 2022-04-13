7129f131448e67ec207304f2f668197007bcbc00
Gabriel Borba <gborba@id.uff.br>
2022-04-13 09:11:54 -0300

    tests skeleton

---
+++ tests/logic/board.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestBoardLogic(unittest.TestCase):
+    def test_king_positions(self) -> None:
+        self.assertEqual(1, 0)  # FIXME
+        return None
+
+    def test_queen_positions(self) -> None:
+        self.assertEqual(1, 0)  # FIXME
+        return None
+
+    def test_rook_positions(self) -> None:
+        self.assertEqual(1, 0)  # FIXME
+        return None
+
+    def test_bishop_positions(self) -> None:
+        self.assertEqual(1, 0)  # FIXME
+        return None
+
+    def test_knight_positions(self) -> None:
+        self.assertEqual(1, 0)  # FIXME
+        return None
+
+    def test_pawn_positions(self) -> None:
+        self.assertEqual(1, 0)  # FIXME
+        return None
