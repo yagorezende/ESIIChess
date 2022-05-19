a70ade4396436f93dc4722910e3f8f4441b9c801
fsouza99 <85891810+fsouza99@users.noreply.github.com>
2022-05-19 17:13:18 -0300

    Delete logic directory

--- logic/tools.py
+++
@@ -1,9 +0,0 @@
-def show_board_matrix(matrix):
-    for row in matrix:
-        for cell in row:
-            if cell:
-                print(f'''{cell : ^5}''', end='')
-            else:
-                print(f'''{'-' : ^5}''', end='')
-        print()
-    return
