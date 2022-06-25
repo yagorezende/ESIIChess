223a505d22a5b0bbc3f994c39e03b9d24e515d34
Gabriel Borba <gborba@id.uff.br>
2022-06-24 23:44:48 -0300

    req: 1.1.4 WIP
    formating and symbol rename

--- ui/screens/game_options.py
+++ ui/screens/game_options.py
@@ -1,14 +1,15 @@
 from typing import List, Tuple
+
 import pygame
 import ui.screens.generic_screen as gs
-import ui.widgets.generic_widget as gw
+from logic.start_game_command import StartGameCommand
 from ui.widgets.radial_selection import RadialSelection
 from ui.widgets.text_button import TextButton
 
 
 class GameOptions(gs.GenericScreen):
     def __init__(self) -> None:
-        self.screen_size = [0,0]
+        self.screen_size = [0, 0]
         self._WIDGETS_PADDING: int = 10
         self.oponent = RadialSelection(
             0, 0,
@@ -20,18 +21,20 @@ class GameOptions(gs.GenericScreen):
             title='Choose your color',
             options=('White', 'Black', 'Random'),
             default=0)
-        self.dificulty = RadialSelection(
+        self.difficulty = RadialSelection(
             0, 0,
-            title='Choose the game dificulty',
+            title='Choose the game difficulty',
             options=('Easy', 'Normal', 'Hard'),
             default=0)
+        self.difficulty.active = not self.oponent.selected_option[0] == 0
         self.button_continue = TextButton(
             0, 0,
             text='Play',
-            bg_color_normal=(30,127,30),
-            bg_color_hover=(30,117,30),
-            bg_color_click=(30,107,30),
-            trbl_padding=(5,20,5,20))
+            bg_color_normal=(30, 127, 30),
+            bg_color_hover=(30, 117, 30),
+            bg_color_click=(30, 107, 30),
+            trbl_padding=(5, 20, 5, 20),
+            click_command=StartGameCommand(game_options_screen=self))
         return None
 
     def on_enter(self) -> None:
@@ -42,9 +45,9 @@ class GameOptions(gs.GenericScreen):
         #     w.on_event(event)
         self.oponent.on_event(event)
         self.color.on_event(event)
-        self.dificulty.active = not self.oponent.selected_option[0] == 0
-        if self.dificulty.active:
-            self.dificulty.on_event(event)
+        self.difficulty.active = not self.oponent.selected_option[0] == 0
+        if self.difficulty.active:
+            self.difficulty.on_event(event)
         self.button_continue.on_event(event)
         return None
 
@@ -58,12 +61,12 @@ class GameOptions(gs.GenericScreen):
         scr_surf.fill((0, 0, 0))
         scr_surf.blit(*self.oponent.on_render())
         scr_surf.blit(*self.color.on_render())
-        if self.dificulty.active:
-            scr_surf.blit(*self.dificulty.on_render())
+        if self.difficulty.active:
+            scr_surf.blit(*self.difficulty.on_render())
         scr_surf.blit(*self.button_continue.on_render())
         return None
 
-    def _update_widgets_position(self,screen_size: Tuple[int,int]) -> None:
+    def _update_widgets_position(self, screen_size: Tuple[int, int]) -> None:
         if screen_size[0] != self.screen_size[0] or screen_size[1] != self.screen_size[1]:
             self.screen_size = list(screen_size)
             # -----   -----
@@ -75,8 +78,8 @@ class GameOptions(gs.GenericScreen):
             self.color.rect.topleft = self.oponent.rect.bottomleft
             self.color.rect.top += self._WIDGETS_PADDING
             # -----   -----
-            self.dificulty.rect.topleft = self.color.rect.bottomleft
-            self.dificulty.rect.top += self._WIDGETS_PADDING
+            self.difficulty.rect.topleft = self.color.rect.bottomleft
+            self.difficulty.rect.top += self._WIDGETS_PADDING
             # -----   -----
             width_diff = self.screen_size[0]-self.button_continue.rect.width
             self.button_continue.rect.left = int(0.5*width_diff)
