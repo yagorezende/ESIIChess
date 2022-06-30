651010c946712eaea0b9a7ec5a120d282250d803
Gabriel Borba <gborba@id.uff.br>
2022-06-30 18:06:33 -0300

    req: tests
    outputing the detailed test result to a file

--- testMain.py
+++ testMain.py
@@ -12,6 +12,9 @@ To generate coverage lcov file:
 python -m coverage lcov
 """
 
+import datetime
+import io
+import os
 import sys
 import unittest
 from typing import Dict, List, Set
@@ -22,7 +25,7 @@ import tests.test_suites as ts
 _suites: Dict[ts.TestSuites, Set[unittest.TestCase]] = {}
 # NOTE - gather tests from modules
 _suites_list: List[Dict[ts.TestSuites, List[unittest.TestCase]]] = [
-        tlr_unit.suites()
+    tlr_unit.suites()
 ]
 # NOTE - extend _suites with _suites_list
 for d in _suites_list:
@@ -33,7 +36,8 @@ for d in _suites_list:
 
 
 def main(argv):
-    runner = unittest.TextTestRunner(verbosity=2)
+    str_out = io.StringIO()
+    runner = unittest.TextTestRunner(stream=str_out, verbosity=2)
     tests = unittest.TestSuite()
     desired_test_suites: Set[ts.TestSuites] = set()
     not_found_test_suites: Set[str] = set()
@@ -44,6 +48,7 @@ def main(argv):
             desired_test_suites.add(ts.TestSuites[upper_name])
         except KeyError:
             not_found_test_suites.add(suite_name)
+    str_suites_not_found = f"""{'+'*70}\nSUITE(S) NOT FOUND:\n{not_found_test_suites}\n{'-'*70}"""
     # -----   -----
     if len(desired_test_suites) == 0:
         for s in _suites:
@@ -51,12 +56,26 @@ def main(argv):
     # -----   -----
     for dts in desired_test_suites:
         tests.addTests(_suites[dts])
-    runner.run(tests)
+    # NOTE - show suites not found on top of the generated test results file
+    if len(not_found_test_suites) > 0:
+        print(str_suites_not_found,'\n',file=str_out)
+    result = runner.run(tests)
+    # -----   -----
+    time_now = datetime.datetime.now()
+    file_name = time_now.strftime('%Y%m%d-%H%M%S')
+    file_directory = './tests/output'
+    if not os.path.isdir(file_directory):
+        os.mkdir(file_directory)
+    with open(file=f'{file_directory}/test-{file_name}.txt',
+              mode='w', encoding='utf-8') as fp:
+        print(str_out.getvalue(), file=fp)
     # -----   -----
     if len(not_found_test_suites) > 0:
-        print(
-            f"""\n{'+'*70}\nSUITE(S) NOT FOUND:\n{not_found_test_suites}\n""")
-
+        print(str_suites_not_found)
+    if not result.wasSuccessful():
+        print(f'Tests ran={result.testsRun} (failures={len(result.failures)+len(result.unexpectedSuccesses)}) (errors={len(result.errors)})')
+    else:
+        print('OK')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
