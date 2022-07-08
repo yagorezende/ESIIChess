79fee9cd3c3730505c97d28ddd3045171fdc50d5
Gabriel Borba <gborba@id.uff.br>
2022-07-08 15:51:59 -0300

    change: end screen
    let the end screen banner be half transparent on click to permit see the game board

--- ui/screens/endgame_screen.py
+++ ui/screens/endgame_screen.py
@@ -23,12 +23,16 @@ class EndGameScreen(GenericScreen):
         self.x = (self.surface.get_width() * .7) / 2 - self.banner.get_width() / 2
         self.y = self.surface.get_height() / 2 - self.banner.get_height() / 2
         self.rect = (self.x, self.y)
+        self._transparency = 255
+        self._initial_surface = surface.copy()
 
     def on_enter(self) -> None:
         return super().on_enter()
 
     def on_event(self, event) -> None:
         super(EndGameScreen, self).on_event(event)
+        if event.type == pygame.MOUSEBUTTONUP:
+            self._transparency = 140 if self._transparency == 255 else 255
         if event.type == pygame.KEYUP and event.unicode == '\r':
             print("Done")
             Navigator().close_actual_screen()
@@ -39,6 +43,8 @@ class EndGameScreen(GenericScreen):
         return super().on_loop()
 
     def on_render(self) -> None:
+        self.surface.blit(self._initial_surface, (0,0))
+        self.banner.set_alpha(self._transparency)
         self.surface.blit(self.banner, self.rect)
 
     def on_leave(self) -> None:
